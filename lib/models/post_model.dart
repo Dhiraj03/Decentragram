8055bc2c8e45b0f3e9b476caa0b7990e08786c39
Dhiraj <dhirajlokesh@gmail.com>
2021-03-22 13:57:41 +0530

    fix: Updated "Post" model

--- lib/models/post_model.dart
+++ lib/models/post_model.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:decentragram/models/comment_model.dart';
 import 'package:flutter/material.dart';
 
 class PostModel {
@@ -11,16 +12,19 @@ class PostModel {
   final bool isImage;
   final DateTime time;
   final bool isLiked;
+  final List<Comment> comments;
   PostModel(
       {@required this.likeCount,
       @required this.caption,
       @required this.commentCount,
       this.image,
       this.text,
+      @required this.comments,
       @required this.isLiked,
       @required this.isImage,
       @required this.time});
-  factory PostModel.imagePost(Map<String, dynamic> json, var image, bool isLiked) {
+  factory PostModel.imagePost(
+      Map<String, dynamic> json, var image, bool isLiked, List<Comment> comments) {
     return PostModel(
         likeCount: json["likeCount"],
         image: image,
@@ -28,14 +32,18 @@ class PostModel {
         commentCount: json["commentCount"],
         isImage: json["isImage"],
         isLiked: isLiked,
-        time: DateTime.parse(json["time"]));
+        time: DateTime.parse(json["time"]),
+        comments: comments
+        );
   }
 
-  factory PostModel.textPost(Map<String, dynamic> json, String text, bool isLiked) {
+  factory PostModel.textPost(
+      Map<String, dynamic> json, String text, bool isLiked, List<Comment> comments) {
     print('lol');
     return PostModel(
-      isLiked: isLiked,
+        isLiked: isLiked,
         likeCount: json["likeCount"],
+        comments: comments,
         caption: json["caption"],
         text: text,
         commentCount: json["commentCount"],
